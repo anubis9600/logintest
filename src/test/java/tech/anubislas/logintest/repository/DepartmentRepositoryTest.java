9e36c503570134f8c38001d74a6fc2fe49f5b7d0
Olivier Rukabo <moiserukabo@gmail.com>
2023-04-17 15:07:57 +0200

    Bonjour test respository

--- src/test/java/tech/anubislas/logintest/repository/DepartmentRepositoryTest.java
+++ src/test/java/tech/anubislas/logintest/repository/DepartmentRepositoryTest.java
@@ -1,13 +1,40 @@
 package tech.anubislas.logintest.repository;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import tech.anubislas.logintest.entity.Department;
+
+@DataJpaTest
 public class DepartmentRepositoryTest {
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @Autowired
+    TestEntityManager entityManager;
 
     @BeforeEach
     void setUp(){
+        Department department = 
+                    Department.builder()
+                    .departmentAdress("Gisenyi")
+                    .departmentCode("MECH-010")
+                    .departmentName("Mechanic")
+                    .build();
+        
+        entityManager.persist(department);
+    }
+
+    @Test
+    public void whenFindById_thenReturnDepartment(){
+        Department department = departmentRepository.findById(3).get();
+        assertEquals(department.getDepartmentName(), "Mechanic");
     }
 
 }
